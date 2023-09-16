386ca3053b6f22d70511c27c806515ec14fb412d
David <okekedavid1333@gmail.com>
2023-09-16 13:17:36 +0100

    Version 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,87 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Store from "./components/Store.vue";
+import Footer from "./components/Footer.vue";
+import { ig, coffee, git, linkedIn, folio, script } from "./assets/index";
+let portfolio = {
+  name: "Portfolio",
+  disc: "Explore my portfolio website to discover a curated collection of my creative work, showcasing my skills in design, development, and more.",
+  link: "https://kundankr.online/",
+  logo: folio,
+};
+let resume = {
+  name: "Resume",
+  disc: "Join our web developer community server on Discord, where you can ask any questions related to web development and help others with their queries. Let's come together to share knowledge and support each other in our web development journey.",
+  link: "https://discord.gg/KYfPpKbZmE",
+  logo: script,
+};
+let buyCoffee = {
+  name: "Buy Me a Coffee",
+  disc: "If you appreciate my work, and you're feeling generous feel free to buy me a cup of coffee. Thank you!",
+  link: "https://www.buymeacoffee.com/kundnkr",
+  logo: coffee,
+};
+let github = {
+  name: "GitHub",
+  disc: "Don't feel like manually copying the code? Grab the code straight from GitHub.",
+  link: "https://github.com/Kundnkr?tab=repositories",
+  logo: git,
+};
+let instagram = {
+  name: "Instagram",
+  disc: "Stay in the loop with my life's adventures by connecting with me on Instagram.",
+  link: "https://www.instagram.com/kundnkr/",
+  logo: ig,
+};
+let linkedin = {
+  name: "YouTube",
+  disc: "Do you want longer & details step-by-step toturial? Subscribe to my Channel.",
+  link: "https://youtube.com/@kundnkr",
+  logo: linkedIn,
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-black text-white">
+    <h1 class="py-6 text-2xl containr">My Links</h1>
+    <hr />
+    <div class="my-4 flex flex-col p-2 rounded-xl text-white gap-6">
+      <Store
+        :title="resume.name"
+        :description="resume.disc"
+        :url="resume.link"
+        :logo="resume.logo"
+      />
+      <Store
+        :title="portfolio.name"
+        :description="portfolio.disc"
+        :url="portfolio.link"
+        :logo="portfolio.logo"
+      />
+      <Store
+        :title="github.name"
+        :description="github.disc"
+        :url="github.link"
+        :logo="github.logo"
+      />
+      <Store
+        :title="linkedin.name"
+        :description="linkedin.disc"
+        :url="linkedin.link"
+        :logo="linkedin.logo"
+      />
+      <Store
+        :title="instagram.name"
+        :description="instagram.disc"
+        :url="instagram.link"
+        :logo="instagram.logo"
+      />
+      <Store
+        :title="buyCoffee.name"
+        :description="buyCoffee.disc"
+        :url="buyCoffee.link"
+        :logo="buyCoffee.logo"
+      />
+    </div>
+    <Footer title="Any questions or remarks?" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { gmail, wapp } from '../assets';
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col items-center border-t py-4 gap-2">
+    <h2 class="text-lg md:text-xl lg:text-2xl font-semibold">{{ title }}</h2>
+    <div class="flex text-xs divide-x-2 w-full justify-center containr md:text-sm lg:text-base">
+        <div class="flex justify-around items-center">
+            <img :src=wapp alt="" class="w-10 mx-4">
+        </div>
+        <div class="flex justify-around items-center">
+            <img :src=gmail alt="" class="w-10 mx-4">            
+        </div>
+    </div>
+  </div>
+</template>
